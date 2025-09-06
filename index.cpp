cff6fe686b4c42c4dd912c849a748ddfdf64dde4
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-06 23:15:51 +0500

    Code : input from user & loop on marks of subjects

--- index.cpp
+++ index.cpp
@@ -5,6 +5,13 @@ int main() {
     float marks, total = 0, average;
     int subjects;
     
-   
+   cout << "Enter the number of subjects: ";
+    cin >> subjects;
+    
+    for(int i = 1; i <= subjects; i++) {
+        cout << "Enter marks for Subject " << i << ": ";
+        cin >> marks;
+        total += marks; }
+    
     
 }
